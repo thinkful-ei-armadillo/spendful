7e95d4dc6d66b54ff210b5a0ffcee0cfb1cceef0
Andre K. W <andre.k.willie@gmail.com>
2019-04-23 19:01:38 -0400

    made chart responsive

--- src/routes/DashboardPage.js
+++ src/routes/DashboardPage.js
@@ -3,27 +3,47 @@ import {Doughnut} from 'react-chartjs-2';
 import IncomeExpenseList from '../components/IncomeExpenseList';
 import './DashboardPage.css';
 
-export default class Dashboard extends Component{
-  render() {
-    const chartData = {
-      labels: ["Groceries", "Fuel", "Bribes"],
-      datasets: [{
-      label: "Total Expenses",
-      backgroundColor: ["#DEB887", "#A9A9A9", "#DC143C"], 
-      borderColor: ["#CDA776", "#989898", "#CB252B"],
-      data: [200, 150, 1000],
-      }]} 
+export default class Dashboard extends Component {
+  state = {
+    chart: {
+      data: {
+        labels: ["Groceries", "Fuel", "Bribes"],
+        datasets: [
+          {
+            label: "Total Expenses",
+            backgroundColor: ["#DEB887", "#A9A9A9", "#DC143C"], 
+            borderColor: ["#CDA776", "#989898", "#CB252B"],
+            data: [200, 150, 1000],
+          }
+        ],
+      },
+      options: {
+        legend: {
+          position: 'bottom',
+          labels: {
+            boxWidth: 12,
+            padding: 30,
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    }
+  }
 
+  render() {
     return (
-      <main>
+      <main className="flex-main">
         <section className="page-controls">
           <select>
             <option>April 2019</option>
           </select>
         </section>
 
+        <div className="w-100"></div>
+
         <section className="page-chart">
-          <Doughnut data={chartData} width={200} height={60}/>        
+          <Doughnut data={this.state.chart.data} options={this.state.chart.options} />
         </section>
 
         <section className="page-summaries">
