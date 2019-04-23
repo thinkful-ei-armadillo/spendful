70ffd9cb8fdbf8d0e9d1700c371283aa5bf015ca
Andre K. W <andre.k.willie@gmail.com>
2019-04-23 14:29:11 -0400

    added links to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import LandingPage from '../routes/LandingPage';
 import LoginPage from '../routes/LoginPage'; 
+import IncomePage from '../routes/IncomePage';
 import './App.css';
 
 class App extends Component {
   render() {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
+    const navLinks = <>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/income">Income</Link>
+      <Link to="/expenses">Expenses</Link>
+    </>;
 
     return <>
       <nav>
         <div>
-          <Link to="/"><h1>Spendful</h1></Link>
-
-          {isLoggedIn
-            ? <Link to="/logout">Logout</Link>
-            : <Link to="/login">Login</Link>}
+          <div className="nav-left">
+            <Link to="/"><h1>Spendful</h1></Link>
+            {isLoggedIn ? navLinks : ''}
+          </div>
+          <div className="nav-right">
+            {isLoggedIn
+              ? <Link to="/logout">Logout</Link>
+              : <Link to="/login">Login</Link>}
+          </div>
         </div>
       </nav>
 
       <div id="app-container">
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/income" component={IncomePage} />
         </Switch>
       </div>
     </>;
