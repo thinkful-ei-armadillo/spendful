6a9b6aaca1b467019a8ca78e09dbabcfeb7e4405
Andre K. W <andre.k.willie@gmail.com>
2019-04-23 11:23:47 -0400

    started wireframing landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import LandingPage from '../routes/LandingPage';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    const isLoggedIn = false;
+
+    return <>
+      <nav>
+        <div>
+          <h1>Spendful</h1>
+
+          {isLoggedIn
+            ? <Link to="/logout">Logout</Link>
+            : <Link to="/login">Login</Link>}
+        </div>
+      </nav>
+
       <div id="app-container">
         <Switch>
           <Route exact path="/" component={LandingPage} />
         </Switch>
       </div>
-    );
+    </>;
   }
 }
 
