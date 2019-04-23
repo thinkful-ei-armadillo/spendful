fd3dff9219017ac202d51cb07ee8a57c3fe43b4a
Andre K. W <andre.k.willie@gmail.com>
2019-04-23 12:37:07 -0400

    added css for login page

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
 
 export default class LoginPage extends Component {
-  
-  render(){
+  render() {
     return(
-      <div>
+      <section className="login-form">
         <form>
           <input type="text" placeholder="Username"/>
           <input type="password" placeholder="Password"/>
           <button type="submit">Log In</button> 
         </form>
-      </div>
+        <Link to="/">Don't have an account?</Link>
+      </section>
     )
   }
 }
