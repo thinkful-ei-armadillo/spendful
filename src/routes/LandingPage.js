90ba4aabe96b00e9e0555eaa46bb0c70dde97fb8
Andre K. W <andre.k.willie@gmail.com>
2019-04-23 12:07:25 -0400

    added header to landing page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import './LandingPage.css';
 
 export default class LandingPage extends Component {
   render() {
-    return (
+    return <>
+      <header>
+        <div className="landing-header">
+          <div className="landing-header-left">
+            <h2>Spendful is the newest way to prevent yourself from becoming broke.</h2>          
+          </div>
+
+          <div className="landing-header-right">
+            <form>
+              <input type="text" placeholder="Email"></input>
+              <input type="text" placeholder="Username"></input>
+              <input type="password" placeholder="Password"></input>
+              <button type="submit">Create an account!</button>
+            </form>
+          </div>
+        </div>
+      </header>
+
       <main>
         <p>Welcome to spendful</p>
       </main>
-    );
+    </>;
   }
 }
