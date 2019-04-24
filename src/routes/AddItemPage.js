409833a3ee12c7c58ede3145b8da2892500179d8
Andre K. W <andre.k.willie@gmail.com>
2019-04-24 15:16:22 -0400

    cleaned up form; added state and use url hash

--- src/routes/AddItemPage.js
+++ src/routes/AddItemPage.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default class AddItemPage extends Component {
+class AddItemPage extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
     this.state = {
-      type: null
+      type: 'category',
+      categories: {
+        income: ['paycheck', 'personal'],
+        expense: ['groceries', 'clothing', 'recreation'],
+      },
+    }
+  }
+
+  componentDidMount() {
+    const validTypes = ['category', 'income', 'expense'];
+    const hash = this.props.location.hash.substr(1);
+
+    if(validTypes.includes(hash)) {
+      this.setState({type: hash});
     }
   }
 
@@ -15,52 +29,57 @@ export default class AddItemPage extends Component {
     })
   }
 
-  renderNewItem = (type) => {
-      return(
-        <div>
-          <select>
-            <option>Groceries</option>
-            <option>Fuel</option>
-            <option>Bribe</option>
-          </select>
-          <input type="date"/>
-          <input type="text" placeholder="Name" />
-          <input type="number" />
-          <select>
-            <option>Bi-weekly</option>
-            <option>Monthly</option>
-          </select>
-          <button type="submit">Create</button>
-        </div>
-      )  
-    }
-    
-    renderNewCategory = () => {
-      return(
-        <div>
-          <input type="text" placeholder="Category Name" />
-          <button type="submit">Submit</button>
-        </div>
-      )
+  renderForm = () => {
+    if(this.state.type === 'category') {
+      return <>
+        <label htmlFor="input-category">Category name</label>
+        <input type="text" id="input-category" />
+        <button type="submit">Submit</button>
+      </>;
     }
 
-  render(){
-    return(
-    <div>
-      <h2>Track A New Item</h2>
-      <form> 
-        <select onChange={this.handleChange}> 
-          <option>Category</option>
-          <option>Income</option>
-          <option>Expense</option>
-        </select>
-        {(this.state.type === 'Income' || this.state.type === 'Expense')
-          ? (this.state.type === 'Income') 
-            ? this.renderNewItem('Income')
-            : this.renderNewItem('Expense')
-          : this.renderNewCategory()}
-      </form>
-    </div>
-    )
+    return <>
+      <label htmlFor="input-type">Type</label>
+      <select id="input-type">
+        {this.state.categories[this.state.type].map(category => <option key={category}>{category}</option>)}
+      </select>
+
+      <label htmlFor="input-date">Date</label>
+      <input type="date" id="input-date" />
+
+      <label htmlFor="input-description">Short description (max 50 chars.)</label>
+      <input type="text" id="input-description" maxLength="50" />
+
+      <label htmlFor="input-amount">Amount</label>
+      <input type="number" id="input-amount" />
+
+      <label htmlFor="input-freq">Frequency</label>
+      <select id="input-freq">
+        <option>Bi-weekly</option>
+        <option>Monthly</option>
+      </select>
+
+      <button type="submit">Create</button>
+    </>;
+  }
+
+  render() {
+    return (
+      <main className="flex-main">
+        <form className="flex-form">
+          <label htmlFor="input-type">I want to add...</label>
+          <select id="input-type" onChange={this.handleChange}> 
+            <option>category</option>
+            {Object.keys(this.state.categories).map(type => 
+              <option key={type} selected={this.state.type === type}>{type}</option>)}
+          </select>
+
+          <hr></hr>
+          {this.renderForm()}
+        </form>
+      </main>
+    );
   }
 }
+
+export default withRouter(AddItemPage);
