2ff430fc24dd9503854b4f9ade546950e4c7043a
Christopher <christopher.carnivale@gmail.com>
2019-04-24 11:31:14 -0400

    began wiring for buttons

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
+import AuthApiService from '../services/auth-api-service';
 
 
 export default class LandingPage extends Component {
+
+  static defaultProps = {
+    history: {
+      push: () => {}, 
+    },
+  }
+
+  handleRegistrationSuccess = () => {
+    const { history } = this.props
+    history.push('/login')
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const {email, username, password } = e.target; 
+    AuthApiService.postUser({
+      name: email.value,
+      username: username.value,
+      password: password.value 
+    })
+    .then( () => {
+      email.value= ''
+      username.value= ''
+      password.value= ''
+      this.handleRegistrationSuccess(); 
+    })
+  }
+
   render() {
     return <>
       <header>
@@ -12,10 +41,10 @@ export default class LandingPage extends Component {
           </div>
 
           <div className="landing-header-right">
-            <form>
-              <input type="text" placeholder="Email"></input>
-              <input type="text" placeholder="Username"></input>
-              <input type="password" placeholder="Password"></input>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="Email" name="email" required></input>
+              <input type="text" placeholder="Username" name="username" required></input>
+              <input type="password" placeholder="Password" name="password" required></input>
               <button type="submit">Create an account!</button>
             </form>
           </div>
