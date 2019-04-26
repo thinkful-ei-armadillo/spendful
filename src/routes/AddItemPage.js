018bb5ca3820ef32a462c7b8b53a94303263b4e0
mdb1710 <mdb1710@gmail.com>
2019-04-26 10:39:56 -0400

    tested console logs

--- src/routes/AddItemPage.js
+++ src/routes/AddItemPage.js
@@ -8,11 +8,16 @@ class AddItemPage extends Component {
     super(props);
 
     this.state = {
+      name: null,
       type: 'income',
+      amount: null,
+      description: '',
+      start_date: '',
       categories: {
-        income: ['paycheck', 'personal'],
-        expense: ['groceries', 'clothing', 'recreation'],
+        income: ['Paycheck', 'Direct Deposit', 'Royalties'],
+        expense: ['Rent', 'Utilites', 'Groceries', 'Auto']
       },
+      recurring_rule: ''
     }
   }
 
@@ -25,30 +30,67 @@ class AddItemPage extends Component {
     }
   }
 
+  handl
+
   handleTypeChange = (event) => {
     this.setState({
       type: event.target.value 
     })
+    console.log(this.state.type)
   }
 
   handleCategoryChange = (e) => {
     if(e.target.value === -1) {
       const newCategory = prompt('Enter the name for the new category:');
-      
-
-      // POST to create category
+       // POST to create category
     }
+    // this.setState({
+    //   categories: e.target.value
+    // })
+    console.log(this.state.categories)
   }
 
-  addNewCategory = () => {
-    const addCategory = 
+  handleAmountChange = (e) => {
+    this.setState({
+      amount: e.target.value
+    })
+    console.log(this.state.amount);
+  }
+
+  handleDateChange = (e) => {
+    this.setState({
+      start_date: e.target.value
+    })
+    console.log(this.state.start_date);
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value
+    })
+    console.log(this.state.description)
+  }
+
+  handleFreqChange = (e) => {
+    this.setState({
+      recurring_rule: e.target.value
+    })
+    console.log(this.state.recurring_rule)
+  }
+
+  addNewItem = () => {
+    const addCategory = {
+      name: test,
+      type: test,
+      monthly_budget: test
+    }
     fetch(`${config.API_ENDPOINT}/categories`, {
       method: 'POST',
       headers: {
         "Authorization": `bearer ${TokenService.getAuthToken()}`,
         "content-type": "application/json"
       },
-      body: JSON.stringify(newCategory)
+      body: JSON.stringify(addCategory)
     }) 
 
   }
@@ -63,16 +105,16 @@ class AddItemPage extends Component {
       </select>
 
       <label htmlFor="input-date">Date</label>
-      <input type="date" id="input-date" />
+      <input type="date" id="input-date" onChange={this.handleDateChange}/>
 
       <label htmlFor="input-description">Short description (max 50 chars.)</label>
-      <input type="text" id="input-description" maxLength="50" />
+      <input type="text" id="input-description" maxLength="50" onChange={this.handleDescriptionChange}/>
 
       <label htmlFor="input-amount">Amount</label>
-      <input type="number" id="input-amount" />
+      <input type="number" id="input-amount" onChange={this.handleAmountChange}/>
 
       <label htmlFor="input-freq">Frequency</label>
-      <select id="input-freq">
+      <select id="input-freq" onChange={this.handleFreqChange}>
         <option>Bi-weekly</option>
         <option>Monthly</option>
       </select>
@@ -84,7 +126,7 @@ class AddItemPage extends Component {
   render() {
     return (
       <main className="flex-main">
-        <form className="flex-form" onSubmit=>
+        <form className="flex-form" onSubmit={this.addNewItem}>
           <label htmlFor="input-type">I want to add...</label>
           <select id="input-type" onChange={this.handleTypeChange} value={this.state.type}>
             {Object.keys(this.state.categories).map(type => 
