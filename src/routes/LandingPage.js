82a006557ffa812566897982d67f91850bebfc32
Andre K. W <andre.k.willie@gmail.com>
2019-04-26 14:04:22 -0400

    fixed #23

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -10,29 +10,40 @@ export default class LandingPage extends Component {
     },
   }
 
+  state = {
+    error: [],
+  }
+
   handleRegistrationSuccess = () => {
     const { history } = this.props
     history.push('/login')
   }
 
   handleSubmit = e => {
-    e.preventDefault()
-    const {email_address, full_name, password } = e.target; 
+    e.preventDefault();
+    this.setState({error: []});
+
+    const {email_address, full_name, password } = e.target;
+
     AuthApiService.postUser({
       email_address: email_address.value,
       full_name: full_name.value,
       password: password.value 
     })
-    .then( () => {
+    .then(() => {
       email_address.value= ''
       full_name.value= ''
       password.value= ''
       this.handleRegistrationSuccess(); 
     })
+    .catch(err => {
+      this.setState({error: err.errors[0]});
+    });
   }
 
   render() {
     return <>
+      {this.state.error.length > 0 ? <div className="alert-error-lg">{this.state.error}</div> : ''}
       <header>
         <div className="landing-header">
           <div className="landing-header-left">
@@ -41,9 +52,12 @@ export default class LandingPage extends Component {
 
           <div className="landing-header-right">
             <form onSubmit={this.handleSubmit}>
-              <input type="text" placeholder="Email" name="email_address" required></input>
-              <input type="text" placeholder="Username" name="full_name" required></input>
-              <input type="password" placeholder="Password" name="password" required></input>
+              <label htmlFor="input_email">Email</label>
+              <input type="text" id="input_email" name="email_address" autoComplete="off" required></input>
+              <label htmlFor="input_name">Full name</label>
+              <input type="text" id="input_name" name="full_name" autoComplete="off" required></input>
+              <label htmlFor="input_password">Password</label>
+              <input type="password" id="input_password" name="password" autoComplete="off" required></input>
               <button type="submit">Create an account!</button>
             </form>
           </div>
