2b14a1679db13627254ae2e6897430ff6adbb6ae
Christopher <christopher.carnivale@gmail.com>
2019-04-29 11:00:19 -0400

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
   TOKEN_KEY: 'spendful-client-auth-token'
 }
 
-//process.env.REACT_APP_API_BASE ||
