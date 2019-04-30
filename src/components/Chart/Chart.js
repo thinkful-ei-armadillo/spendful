2ef9b1b71b606105a91183b46734905fcecf202e
Andre K. W <andre.k.willie@gmail.com>
2019-04-30 16:14:05 -0400

    created chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2'; 
+
+
+export default class Chart extends Component {
+  state = {
+    categoryColors: {
+      0: '#f04511',
+      1: '#5501d0',
+      2: '#ff01fa',
+      3: '#508fe4',
+      4: '#0fa931',
+    },
+    chart: {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Total Expenses",
+            backgroundColor: [],
+            data: [],
+          }
+        ],
+      },
+      options: {
+        legend: {
+          position: 'bottom',
+          labels: {
+            boxWidth: 12,
+            padding: 30,
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    },
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.data.incomes.length !== this.props.data.incomes.length) {
+      this.updateChart();
+    }
+  }
+
+  updateChart = () => {
+    let chart = this.state.chart;
+    let data = [];
+    let labels = [];
+    let backgroundColor = [];
+
+    // this is why the chart doesnt render the data properly
+    this.props.data.expenses.forEach(item => {
+      data.push(parseInt(item.amount));
+      labels.push(this.props.data.categories.find(c => c.id === item.category_id).name);
+      backgroundColor.push(this.state.categoryColors[item.category_id] || this.state.categoryColors[0]);
+    });
+
+    chart.data.labels = labels;
+    chart.data.datasets[0].data = data;
+    chart.data.datasets[0].backgroundColor = backgroundColor;
+
+    this.setState({chart});
+  }
+
+  render() {
+    return (
+      <section className="page-chart">
+        <Doughnut data={this.state.chart.data} options={this.state.chart.options} />
+      </section>
+    );
+  }
+}
--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,50 +1,19 @@
 import React, { Component } from 'react'; 
-import {Doughnut} from 'react-chartjs-2'; 
 import IncomeExpenseList from '../IncomeExpenseList';
-import './DashboardPage.css';
-import config from '../../config'
-import TokenService from '../../services/token-service'
+import Chart from '../Chart/Chart';
 import DataContext from '../../contexts/DataContext';
 import { getAllCategories } from '../../services/categories-service';
 import { getMonthlyReport } from '../../services/reports-service';
+import './DashboardPage.css';
 
 
 export default class DashboardPage extends Component {
   static contextType = DataContext;
 
   state = {
-    categoryColors: {
-      0: '#f04511',
-      1: '#5501d0',
-      2: '#ff01fa',
-      3: '#508fe4',
-      4: '#0fa931',
-    },
-    chart: {
-      data: {
-        labels: [],
-        datasets: [
-          {
-            label: "Total Expenses",
-            backgroundColor: [],
-            data: [],
-          }
-        ],
-      },
-      options: {
-        legend: {
-          position: 'bottom',
-          labels: {
-            boxWidth: 12,
-            padding: 30,
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-      }
-    },
     incomes: [], 
-    expenses: [] 
+    expenses: [],
+    categories: [],
   }
 
   componentDidMount() {
@@ -53,28 +22,6 @@ export default class DashboardPage extends Component {
         this.setState({categories});
         this.handleReports(2019, 4)
       })
-  }
-
-  updateChart = () => {
-    let chart = this.state.chart;
-    let data = [];
-    let labels = [];
-    let backgroundColor = [];
-
-    console.log(this.state.expenses);
-
-    // this is why the chart doesnt render the data properly
-    this.state.expenses.forEach(item => {
-      data.push(parseInt(item.amount));
-      labels.push(this.state.categories.find(c => c.id === item.category_id).name);
-      backgroundColor.push(this.state.categoryColors[item.category_id] || this.state.categoryColors[0]);
-    });
-
-    chart.data.labels = labels;
-    chart.data.datasets[0].data = data;
-    chart.data.datasets[0].backgroundColor = backgroundColor;
-
-    this.setState({chart});
   }
 
   handleReports = (year, month) => {
@@ -90,10 +37,11 @@ export default class DashboardPage extends Component {
     getMonthlyReport(year, month)
       .then(report => {
         this.setState({incomes: report.incomes, expenses: report.expenses});
-        this.updateChart();
       })
   }
 
+  // chart is receiving the whole state, which includes
+  // incomes, expenses, and catagories
   render() {
     return (
       <main className="flex-main">
@@ -105,9 +53,7 @@ export default class DashboardPage extends Component {
 
         <div className="w-100"></div>
 
-        <section className="page-chart">
-          <Doughnut data={this.state.chart.data} options={this.state.chart.options} />
-        </section>
+        <Chart data={this.state} />
 
         <section className="page-summaries">
           <IncomeExpenseList type="income" data={this.state.incomes} onlyShowRecent />
