046c01d8f3f3c02577c6bc4e9e4fc9d1809a9df3
Christopher <christopher.carnivale@gmail.com>
2019-05-01 15:02:44 -0400

    cleaned up comments

--- src/components/AddItemPage/AddItemPage.js
+++ src/components/AddItemPage/AddItemPage.js
@@ -26,19 +26,11 @@ class AddItemPage extends Component {
         {this.state.errors.length > 0 
         ? <div className="alert-error">{this.state.errors}</div>
         : ''}
-          {/* <label htmlFor="input-type">I want to add an...</label>
-          <select id="input-type" onChange={this.handleTypeChange} value={this.state.type}>
-            <option value="income">Income</option>
-            <option value="expense">Expense</option>
-            {Object.keys(this.state.categories).map(type => 
-              <option key={type}>{type}</option>)} 
-          </select> */}
           <AddItemForm 
             itemType={this.props.itemType}
             onSuccess={this.onSuccess}
             onFailure={this.onFailure}
             />
-          {/* {this.renderForm()} */}
       </main>
     )
   }
